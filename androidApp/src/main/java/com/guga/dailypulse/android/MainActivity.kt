b10e69832b0192be28889c8d3816b875b716d76c
gugapadilha <guga.santospadilha@rede.ulbra.br>
2024-09-10 13:58:53 -0300

    testing platform logic

--- androidApp/src/main/java/com/guga/dailypulse/android/MainActivity.kt
+++ androidApp/src/main/java/com/guga/dailypulse/android/MainActivity.kt
@@ -3,12 +3,17 @@ package com.guga.dailypulse.android
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import com.guga.dailypulse.Platform
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        Platform().logSystemInfo()
+
         setContent {
             MyApplicationTheme {
+
             }
         }
     }
