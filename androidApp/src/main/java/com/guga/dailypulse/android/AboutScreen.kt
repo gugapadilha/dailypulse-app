d7adec3d531f03a6c7bb976e76c209ac26615eea
gugapadilha <guga.santospadilha@rede.ulbra.br>
2024-09-10 14:21:04 -0300

    about screen successfully done

--- androidApp/src/main/java/com/guga/dailypulse/android/AboutScreen.kt
+++ androidApp/src/main/java/com/guga/dailypulse/android/AboutScreen.kt
@@ -2,12 +2,19 @@ package com.guga.dailypulse.android
 
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Divider
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 import com.guga.dailypulse.Platform
 
 
@@ -32,13 +39,12 @@ private fun Toolbar() {
 @Composable
 private fun ContentView(){
     val items = makeItems()
-    
-    LazyColumn(modifier = Modifier.fillMaxSize(), ){
-        items(items){
-            RowVi
+
+    LazyColumn(modifier = Modifier.fillMaxSize()) {
+        items(items) { row ->
+            RowView(title = row.first, subtitle = row.second)
         }
     }
-    
 }
 
 private fun makeItems(): List<Pair<String, String>>{
@@ -50,4 +56,23 @@ private fun makeItems(): List<Pair<String, String>>{
         Pair("Device", platform.deviceModel),
         Pair("Density", platform.density.toString())
     )
+}
+
+@Composable
+private fun RowView(
+    title: String,
+    subtitle: String
+){
+    Column(modifier = Modifier.fillMaxWidth()) {
+        Column(modifier = Modifier.padding(8.dp)) {
+            Text(
+                text = title,
+                style = MaterialTheme.typography.bodySmall,
+                color = Color.Gray
+            )
+            Text(text = subtitle, style = MaterialTheme.typography.bodyLarge )
+        }
+        Divider()
+    }
+
 }
