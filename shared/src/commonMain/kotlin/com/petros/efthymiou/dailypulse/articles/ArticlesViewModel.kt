8ac8e81781a1f75431d63aebca8ceb03bfeeb2d2
gugapadilha <guga.santospadilha@rede.ulbra.br>
2024-09-12 13:47:41 -0300

    create both variables to handle mutable and immutable state flow

--- shared/src/commonMain/kotlin/com/petros/efthymiou/dailypulse/articles/ArticlesViewModel.kt
+++ shared/src/commonMain/kotlin/com/petros/efthymiou/dailypulse/articles/ArticlesViewModel.kt
@@ -1,9 +1,14 @@
 package com.petros.efthymiou.dailypulse.articles
 
 import com.petros.efthymiou.dailypulse.BaseViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 
 class ArticlesViewModel: BaseViewModel() {
 
-    val articleState: StateFlow<ArticlesState>
+    //will be the information between interface and ui - mutable state flow
+    private val _articleState: MutableStateFlow<ArticlesState> = MutableStateFlow(ArticlesState())
+
+    //immutable state flow
+    val articlesState: StateFlow<ArticlesState> get() = _articleState
 }
