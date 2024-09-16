0160c8cb89e0bd7f13712bcdcf72c9875d71333a
gugapadilha <guga.santospadilha@rede.ulbra.br>
2024-09-16 14:40:42 -0300

    article screen done

--- androidApp/src/main/java/com/petros/efthymiou/dailypulse/android/screens/ArticleScreen.kt
+++ androidApp/src/main/java/com/petros/efthymiou/dailypulse/android/screens/ArticleScreen.kt
@@ -11,10 +11,17 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.Info
 import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -31,13 +38,33 @@ import com.petros.efthymiou.dailypulse.articles.ArticlesViewModel
 fun ArticleScreen(
     articlesViewModel: ArticlesViewModel
 ) {
-    val articleState = articlesViewModel
+    val articleState = articlesViewModel.articlesState.collectAsState()
 
     Column {
         AppBar()
-
+        if (articleState.value.loading)
+            Loader()
+        if (articleState.value.error != null)
+            ErrorMessage(message = articleState.value.error!!)
+        if (articleState.value.articles.isNotEmpty())
+            ArticleListView(articles = articlesViewModel.articlesState.value.articles)
     }
 
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+private fun AppBar(
+) {
+    TopAppBar(
+        title = { Text(text = "Articles") },
+        actions = {
+                Icon(
+                    imageVector = Icons.Outlined.Info,
+                    contentDescription = "About Device Button",
+                )
+        }
+    )
 }
 
 @Composable
