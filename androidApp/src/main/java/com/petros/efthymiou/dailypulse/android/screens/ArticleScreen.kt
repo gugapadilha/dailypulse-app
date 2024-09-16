5d038dd6a5e87b2a615eb6e26135557c2e0048af
gugapadilha <guga.santospadilha@rede.ulbra.br>
2024-09-16 14:31:01 -0300

    finished articleItemView fun to display cards article with all informations on recycler view

--- androidApp/src/main/java/com/petros/efthymiou/dailypulse/android/screens/ArticleScreen.kt
+++ androidApp/src/main/java/com/petros/efthymiou/dailypulse/android/screens/ArticleScreen.kt
@@ -1,10 +1,23 @@
 package com.petros.efthymiou.dailypulse.android.screens
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import coil.compose.AsyncImage
 import com.petros.efthymiou.dailypulse.articles.Article
 import com.petros.efthymiou.dailypulse.articles.ArticlesViewModel
 
@@ -30,4 +43,28 @@ fun ArticleListView(articles: List<Article>) {
         }
     }
 
+}
+
+@Composable
+fun ArticleItemView(article: Article) {
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(16.dp)
+    ) {
+        AsyncImage(model = article.imageUrl, contentDescription = null)
+        Spacer(modifier = Modifier.height(4.dp))
+        Text(text = article.title,
+            style = TextStyle(fontWeight = FontWeight.Bold, fontSize = 22.sp)
+            )
+        Spacer(modifier = Modifier.height(8.dp))
+        Text(text = article.desc)
+        Spacer(modifier = Modifier.height(4.dp))
+        Text(
+            text = article.date,
+            style = TextStyle(color = Color.Gray),
+            modifier = Modifier.align(Alignment.End)
+        )
+        Spacer(modifier = Modifier.height(4.dp))
+    }
 }
