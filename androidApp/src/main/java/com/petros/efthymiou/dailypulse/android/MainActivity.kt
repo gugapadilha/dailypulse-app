f50447216168183fca8c8daed0548630ef877efd
gugapadilha <guga.santospadilha@rede.ulbra.br>
2024-09-17 13:48:15 -0300

    create variable to viewmodels

--- androidApp/src/main/java/com/petros/efthymiou/dailypulse/android/MainActivity.kt
+++ androidApp/src/main/java/com/petros/efthymiou/dailypulse/android/MainActivity.kt
@@ -3,6 +3,7 @@ package com.petros.efthymiou.dailypulse.android
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.viewModels
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
@@ -10,11 +11,15 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.petros.efthymiou.dailypulse.Platform
 import com.petros.efthymiou.dailypulse.android.screens.AboutScreen
+import com.petros.efthymiou.dailypulse.android.screens.ArticleScreen
+import com.petros.efthymiou.dailypulse.articles.ArticlesViewModel
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         val platform = Platform()
+        val articleViewModel: ArticlesViewModel by viewModels()
 
         setContent {
             MyApplicationTheme {
@@ -24,7 +29,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    AboutScreen()
+                    ArticleScreen(articlesViewModel = )
                 }
             }
         }
