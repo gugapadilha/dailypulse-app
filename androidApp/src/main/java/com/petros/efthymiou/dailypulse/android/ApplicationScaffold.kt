dda47504b3c9076cf080b91f83bf9ddf02cbf777
gugapadilha <guga.santospadilha@rede.ulbra.br>
2024-09-18 14:12:24 -0300

    create fun to navigate throught both screens

--- androidApp/src/main/java/com/petros/efthymiou/dailypulse/android/ApplicationScaffold.kt
+++ androidApp/src/main/java/com/petros/efthymiou/dailypulse/android/ApplicationScaffold.kt
@@ -5,7 +5,13 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import com.petros.efthymiou.dailypulse.android.screens.AboutScreen
+import com.petros.efthymiou.dailypulse.android.screens.ArticleScreen
+import com.petros.efthymiou.dailypulse.android.screens.Screens
 import com.petros.efthymiou.dailypulse.articles.ArticlesViewModel
 
 @Composable
@@ -15,9 +21,30 @@ fun AppScaffold(articlesViewModel: ArticlesViewModel) {
     Scaffold {
         AppNavHost(
             navController = navController,
-            modifier = Modifier.fillMaxSize().padding(it),
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(it),
             articlesViewModel
         )
+    }
+}
+
+@Composable
+fun AppNavHost(
+    navController: NavHostController,
+    modifier: Modifier = Modifier,
+    articlesViewModel: ArticlesViewModel
+) {
+    NavHost(
+        navController = navController, startDestination = Screens.ARTICLES.route,
+        modifier = modifier,
+    ) {
+        composable(Screens.ARTICLES.route){
+            ArticleScreen(onAboutButtonClick = { navController.navigate(Screens.ABOUT_DEVICE.route) }, articlesViewModel = articlesViewModel)
+        }
+        composable(Screens.ABOUT_DEVICE.route){
+            AboutScreen(onUpButtonClick = {navController.popBackStack()})
+        }
     }
 
 }
