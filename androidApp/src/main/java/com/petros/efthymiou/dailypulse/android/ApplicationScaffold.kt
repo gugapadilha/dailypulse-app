bce07cccf7b2c7cca4f75be430467d02f714bd12
gugapadilha <guga.santospadilha@rede.ulbra.br>
2024-09-18 14:08:31 -0300

    create app scaffold fun in application scaffold

---
+++ androidApp/src/main/java/com/petros/efthymiou/dailypulse/android/ApplicationScaffold.kt
@@ -0,0 +1,23 @@
+package com.petros.efthymiou.dailypulse.android
+
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.compose.rememberNavController
+import com.petros.efthymiou.dailypulse.articles.ArticlesViewModel
+
+@Composable
+fun AppScaffold(articlesViewModel: ArticlesViewModel) {
+    val navController = rememberNavController()
+
+    Scaffold {
+        AppNavHost(
+            navController = navController,
+            modifier = Modifier.fillMaxSize().padding(it),
+            articlesViewModel
+        )
+    }
+
+}
