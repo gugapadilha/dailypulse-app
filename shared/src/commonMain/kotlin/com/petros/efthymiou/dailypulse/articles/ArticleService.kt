fae1cd8fc0b42eacec320328c8873bfde42764c3
gugapadilha <guga.santospadilha@rede.ulbra.br>
2024-09-19 14:11:55 -0300

    create fetch articles to set response based on country and category from api

--- shared/src/commonMain/kotlin/com/petros/efthymiou/dailypulse/articles/ArticleService.kt
+++ shared/src/commonMain/kotlin/com/petros/efthymiou/dailypulse/articles/ArticleService.kt
@@ -1,8 +1,18 @@
 package com.petros.efthymiou.dailypulse.articles
 
 import io.ktor.client.HttpClient
+import io.ktor.client.call.body
+import io.ktor.client.request.get
 
-class ArticleService(
-    private val httpClient: HttpClient
-) {
+class ArticleService(private val httpClient: HttpClient) {
+
+    private val country = "us"
+    private val category = "business"
+    private val apiKey = "a37830dd20da499ebf4c4251a8b11590"
+
+    suspend fun fetchArticles(): List<ArticleRaw>{
+        val response: ArticlesResponse = httpClient.get("https://newsapi.org/v2/top-headlines?country=$country&category=$category&apiKey=$apiKey").body()
+        return response.articles
+    }
+
 }
