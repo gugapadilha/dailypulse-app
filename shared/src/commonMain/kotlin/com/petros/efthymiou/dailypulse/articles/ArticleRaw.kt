ae02182ba469935c36c732d7930a73e9aaade742
gugapadilha <guga.santospadilha@rede.ulbra.br>
2024-09-23 14:00:44 -0300

    accessing all articles from the actual api

--- shared/src/commonMain/kotlin/com/petros/efthymiou/dailypulse/articles/ArticleRaw.kt
+++ shared/src/commonMain/kotlin/com/petros/efthymiou/dailypulse/articles/ArticleRaw.kt
@@ -6,11 +6,11 @@ import kotlinx.serialization.Serializable
 @Serializable
 data class ArticleRaw(
     @SerialName("title")
-    val title: String,
+    val title: String?,
     @SerialName("description")
     val desc: String?,
     @SerialName("publishedAt")
-    val date: String,
-    @SerialName("UrlToImage")
+    val date: String?,
+    @SerialName("urlToImage")
     val imageUrl: String?
 )
--- shared/src/commonMain/kotlin/com/petros/efthymiou/dailypulse/articles/ArticlesUseCase.kt
+++ shared/src/commonMain/kotlin/com/petros/efthymiou/dailypulse/articles/ArticlesUseCase.kt
@@ -9,9 +9,9 @@ class ArticlesUseCase(private val service: ArticleService) {
 
     private fun mapArticles(articlesRaw: List<ArticleRaw>): List<Article> = articlesRaw.map { raw ->
         Article(
-            raw.title,
+            raw.title ?: "",
             raw.desc ?: "Click to find out more",
-            raw.date,
+            raw.date?: "",
             raw.imageUrl?: "https://ovicio.com.br/wp-content/uploads/2020/09/20200925-sword-art.jpg"
         )
     }
