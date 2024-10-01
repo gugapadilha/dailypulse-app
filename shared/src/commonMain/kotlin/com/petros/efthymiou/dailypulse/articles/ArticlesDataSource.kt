5e8025d2d8653f896d4f2080eeaa03efaa497ffd
gugapadilha <guga.santospadilha@rede.ulbra.br>
2024-10-01 14:26:06 -0300

    create fun to map the properties and returning themselfs

--- shared/src/commonMain/kotlin/com/petros/efthymiou/dailypulse/articles/ArticlesDataSource.kt
+++ shared/src/commonMain/kotlin/com/petros/efthymiou/dailypulse/articles/ArticlesDataSource.kt
@@ -5,5 +5,18 @@ import petros.efthymiou.dailypulse.db.DailyPulseDatabase
 class ArticlesDataSource(private val database: DailyPulseDatabase) {
 
     fun getAllArticles() : List<ArticleRaw> =
-        database.dailyPulseDatabaseQueries
+        database.dailyPulseDatabaseQueries.selectAllArtitcles(::)
+
+    private fun mapToArticleRaw(
+        title: String,
+        desc: String?,
+        date: String,
+        url: String
+    ) : ArticleRaw =
+        ArticleRaw(
+            title,
+            desc,
+            date,
+            url
+        )
 }
