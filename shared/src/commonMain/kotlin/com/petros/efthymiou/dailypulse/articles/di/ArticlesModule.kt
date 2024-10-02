58a18c2558e612c8cdbbc40ce0695aa16ed164b3
gugapadilha <guga.santospadilha@rede.ulbra.br>
2024-10-02 12:06:24 -0300

    create both article data source and article repository in articles module to access the function

--- shared/src/commonMain/kotlin/com/petros/efthymiou/dailypulse/articles/di/ArticlesModule.kt
+++ shared/src/commonMain/kotlin/com/petros/efthymiou/dailypulse/articles/di/ArticlesModule.kt
@@ -2,6 +2,8 @@ package com.petros.efthymiou.dailypulse.articles.di
 
 import com.petros.efthymiou.dailypulse.articles.Article
 import com.petros.efthymiou.dailypulse.articles.ArticleService
+import com.petros.efthymiou.dailypulse.articles.ArticlesDataSource
+import com.petros.efthymiou.dailypulse.articles.ArticlesRepository
 import com.petros.efthymiou.dailypulse.articles.ArticlesUseCase
 import com.petros.efthymiou.dailypulse.articles.ArticlesViewModel
 import org.koin.dsl.module
@@ -12,4 +14,6 @@ val articleModule = module {
     single<ArticleService> { ArticleService(get()) }
     single<ArticlesUseCase> { ArticlesUseCase(get()) }
     single<ArticlesViewModel> { ArticlesViewModel(get())}
+    single<ArticlesDataSource> {ArticlesDataSource(get())}
+    single<ArticlesRepository> {ArticlesRepository(get(), get())}
 }
