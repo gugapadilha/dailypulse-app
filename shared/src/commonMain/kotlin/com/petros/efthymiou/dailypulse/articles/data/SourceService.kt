bf8601d0b2baaefa7fefdb0d88d24934c0899236
gugapadilha <guga.santospadilha@rede.ulbra.br>
2024-10-07 12:35:40 -0300

    fix source routes

--- shared/src/commonMain/kotlin/com/petros/efthymiou/dailypulse/articles/data/SourceService.kt
+++ shared/src/commonMain/kotlin/com/petros/efthymiou/dailypulse/articles/data/SourceService.kt
@@ -7,9 +7,11 @@ import io.ktor.client.request.get
 class SourceService(private val httpClient: HttpClient) {
 
     private val apiKey = "a37830dd20da499ebf4c4251a8b11590"
+    private val sources = "sources"
+
 
     suspend fun fetchSources(): List<ArticleRaw>{
-        val response: ArticlesResponse = httpClient.get("https://newsapi.org/v2/top-headlines/sources?apiKey=API_KEY").body()
+        val response: ArticlesResponse = httpClient.get("https://newsapi.org/v2/top-headlines?sources=$sources&apiKey=$apiKey").body()
         return response.articles
     }
 }
