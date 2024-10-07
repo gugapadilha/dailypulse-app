98ac2783f6e4820c1cfea03a671fc3c6e8e8b83b
gugapadilha <guga.santospadilha@rede.ulbra.br>
2024-10-07 12:18:19 -0300

    create source repository class

--- shared/src/commonMain/kotlin/com/petros/efthymiou/dailypulse/articles/data/SourceRepository.kt
+++ shared/src/commonMain/kotlin/com/petros/efthymiou/dailypulse/articles/data/SourceRepository.kt
@@ -3,4 +3,8 @@ package com.petros.efthymiou.dailypulse.articles.data
 class SourceRepository(
     private val service: ArticleService
 ) {
+
+    private suspend fun fetchSource(): List<ArticleRaw> {
+        return service.fetchArticles()
+    }
 }
