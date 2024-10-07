1386f4cee3868704daf2f3f3000b73cb3669eebf
gugapadilha <guga.santospadilha@rede.ulbra.br>
2024-10-07 12:11:11 -0300

    create source to implement new feature

---
+++ shared/src/commonMain/kotlin/com/petros/efthymiou/dailypulse/articles/domain/Source.kt
@@ -0,0 +1,9 @@
+package com.petros.efthymiou.dailypulse.articles.domain
+
+data class Source(
+    val id: String?,
+    val name: String?,
+    val desc: String?,
+    val language: String,
+    val country: String
+)
