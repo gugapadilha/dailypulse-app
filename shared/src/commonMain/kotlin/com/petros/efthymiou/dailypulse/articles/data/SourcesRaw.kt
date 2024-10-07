1b5fb8e7e41a5d5e3efbaa2fe1ffa37657d00003
gugapadilha <guga.santospadilha@rede.ulbra.br>
2024-10-07 12:26:33 -0300

    create sourcesraw

---
+++ shared/src/commonMain/kotlin/com/petros/efthymiou/dailypulse/articles/data/SourcesRaw.kt
@@ -0,0 +1,18 @@
+package com.petros.efthymiou.dailypulse.articles.data
+
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class SourcesRaw(
+    @SerialName("id")
+    val id: String?,
+    @SerialName("name")
+    val name: String?,
+    @SerialName("description")
+    val desc: String?,
+    @SerialName("language")
+    val language: String?,
+    @SerialName("country")
+    val country: String?,
+)
